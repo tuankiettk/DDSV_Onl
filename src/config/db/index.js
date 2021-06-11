9248f31b1ede75d779078abd729d522cab2de45b
tuankiettk <tuankietkg127@outlook.com>
2021-06-11 15:26:34 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect(){
 
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/DDSV_Online', {
+        await mongoose.connect('mongodb+srv://tuankiettk:tuankiet3008@cluster0.65hvy.mongodb.net/DDSV_Online', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
